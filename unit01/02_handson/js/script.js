8e2760f18bccde80cbee3c266e92c2df5503e076
Nathalia Rippel <nathalia.rippel@gmail.com>
2020-05-27 09:19:37 -0300

    Refactor HandsOn exercise and use Materialize.

--- unit01/02_handson/js/script.js
+++ unit01/02_handson/js/script.js
@@ -1,67 +1,85 @@
-window.addEventListener('load', afterLoad);
-var redValue = 0;
-var greenValue = 0;
-var blueValue = 0;
+let redValue = 0;
+let greenValue = 0;
+let blueValue = 0;
 
-function afterLoad() {
-  console.log('Page finished loading');
-
-  preventFormSubmit();
-
-  initializeColorBox();
-}
-
-function preventFormSubmit() {
-  var form = document.querySelector('form');
-  form.addEventListener('submit', (event) => {
+window.addEventListener('load', () => {
+  // prevent form submit
+  document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
   });
-}
 
-function initializeColorBox() {
-  // initialize UI
-  document.getElementById('range-red').value = redValue;
-  document.getElementById('number-red').value = redValue;
-  document.getElementById('range-green').value = greenValue;
-  document.getElementById('number-green').value = greenValue;
-  document.getElementById('range-blue').value = blueValue;
-  document.getElementById('number-blue').value = blueValue;
+  handleEvents();
 
-  applyColor(redValue, greenValue, blueValue);
+  initializeColorBox();
+});
 
+const handleEvents = () => {
   // initialize sliders' event listeners
   document
-    .getElementById('range-red')
+    .querySelector('#range-red')
     .addEventListener('input', updateColorBox);
   document
-    .getElementById('range-green')
+    .querySelector('#range-green')
     .addEventListener('input', updateColorBox);
   document
-    .getElementById('range-blue')
+    .querySelector('#range-blue')
     .addEventListener('input', updateColorBox);
-}
+};
 
-function updateColorBox(event) {
+const updateColorBox = (event) => {
   if (event.target.id === 'range-red') {
     redValue = event.target.value;
-    document.getElementById('number-red').value = redValue;
+    document.querySelector('#number-red').value = redValue;
   } else if (event.target.id === 'range-green') {
     greenValue = event.target.value;
-    document.getElementById('number-green').value = greenValue;
+    document.querySelector('#number-green').value = greenValue;
   } else if (event.target.id === 'range-blue') {
     blueValue = event.target.value;
-    document.getElementById('number-blue').value = blueValue;
+    document.querySelector('#number-blue').value = blueValue;
   }
 
   applyColor(redValue, greenValue, blueValue);
-}
+};
 
-function applyColor(r, g, b) {
-  // let rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
-  let rgb = `rgb(${r},${g}, ${b})`;
-  document.getElementById('color-box').style.backgroundColor = rgb;
-  let number = r;
+const initializeColorBox = () => {
+  // initialize UI
+  document.querySelector('#range-red').value = redValue;
+  document.querySelector('#number-red').value = redValue;
+  document.querySelector('#range-green').value = greenValue;
+  document.querySelector('#number-green').value = greenValue;
+  document.querySelector('#range-blue').value = blueValue;
+  document.querySelector('#number-blue').value = blueValue;
 
-  //let hexa = `#${r}${g}${b}`;
-  //document.getElementById('hexa').innerHTML = hexa;
-}
+  applyColor(redValue, greenValue, blueValue);
+};
+
+const applyColor = (r, g, b) => {
+  document.querySelector(
+    '#color-box'
+  ).style.backgroundColor = `rgb(${r},${g}, ${b})`;
+
+  let hexaText = document.querySelector('#hexa');
+  hexaText.innerHTML = `#${getHexa(r)}${getHexa(g)}${getHexa(b)}`;
+
+  // change the text color to appear in front of the background
+  // http://stackoverflow.com/a/3943023/112731
+  // https://www.w3.org/TR/WCAG20/#relativeluminancedef
+  let rlRed = getRelativeLuminance(parseInt(r, 10));
+  let rlGreen = getRelativeLuminance(parseInt(g, 10));
+  let rlBlue = getRelativeLuminance(parseInt(b, 10));
+  hexaText.style.color =
+    0.2126 * rlRed + 0.7152 * rlGreen + 0.0722 * rlBlue > 0.179
+      ? '#000000'
+      : '#FFFFFF';
+};
+
+const getHexa = (value) => {
+  // https://css-tricks.com/converting-color-spaces-in-javascript/
+  let hexa = parseInt(value, 10).toString(16).toUpperCase();
+  return hexa.length === 1 ? '0' + hexa : hexa;
+};
+
+const getRelativeLuminance = (color) => {
+  color /= 255;
+  return color <= 0.03928 ? color / 12.92 : ((color + 0.055) / 1.055) ** 2.4;
+};
