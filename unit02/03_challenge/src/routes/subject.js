eb96c4b5ef97ad9556bea1ba5bcd02294820be5b
Nathalia Rippel <nathalia.rippel@gmail.com>
2020-06-09 22:42:42 -0300

    Implement route to return the best 3 grades of a subject of a given type.

--- unit02/03_challenge/src/routes/subject.js
+++ unit02/03_challenge/src/routes/subject.js
@@ -34,3 +34,38 @@ subjectRouter.get('/avg', async (req, res) => {
     res.status(status).send({ error: err.message });
   }
 });
+
+subjectRouter.get('/top3', async (req, res) => {
+  let status = 200;
+  let params = req.body;
+
+  try {
+    const json = await read();
+
+    const grades = json.grades.filter(
+      (grade) => grade.type === params.type && grade.subject === params.subject
+    );
+
+    if (!grades || !grades.length) {
+      status = 404;
+      throw new Error(
+        `GET /subject/avg - grades not found - params: '${JSON.stringify(
+          params
+        )}'`
+      );
+    }
+
+    grades.sort((a, b) => b.value - a.value);
+
+    let top3List = [];
+    for (let idx = 0; idx < 3; idx++) {
+      top3List.push(grades[idx]);
+    }
+
+    res.send(top3List);
+  } catch (err) {
+    if (status == 200) status = 500;
+    logger.error(err);
+    res.status(status).send({ error: err.message });
+  }
+});
