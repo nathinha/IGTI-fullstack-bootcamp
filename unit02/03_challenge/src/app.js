9e679fce5154564047e4bd31d5761e139f1d94a7
Nathalia Rippel <nathalia.rippel@gmail.com>
2020-06-09 22:20:19 -0300

    Implement route to return sum of grades of a given student and subject.

--- unit02/03_challenge/src/app.js
+++ unit02/03_challenge/src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { gradesRouter } from './routes/grades.js';
+import { studentRouter } from './routes/student.js';
 
 export const app = express();
 
 app.use(express.json());
 app.use('/grades', gradesRouter);
+app.use('/student', studentRouter);
---
+++ unit02/03_challenge/src/routes/student.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { logger } from '../util/logger.js';
+import { read } from '../util/file.js';
+
+export const studentRouter = express.Router();
+
+studentRouter.get('/', async (req, res) => {
+  let status = 200;
+  let params = req.body;
+
+  try {
+    const json = await read();
+
+    const grades = json.grades.filter(
+      (grade) =>
+        grade.student === params.student && grade.subject === params.subject
+    );
+
+    if (!grades || !grades.length) {
+      status = 404;
+      throw new Error(
+        `GET /student - grades not found - student: '${params.student}'`
+      );
+    }
+
+    const sum = grades.reduce((acc, cur) => acc + cur.value, 0);
+
+    res.send({ sum: sum });
+  } catch (err) {
+    if (status == 200) status = 500;
+    logger.error(err);
+    res.status(status).send({ error: err.message });
+  }
+});
