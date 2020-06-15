145a12cd484e65b55e740d77eb55b1362aa9215d
Nathalia Rippel <nathalia.rippel@gmail.com>
2020-06-15 15:46:05 -0300

    Create react-salario base.

---
+++ unit03/react-salario/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'materialize-css/dist/css/materialize.min.css';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
