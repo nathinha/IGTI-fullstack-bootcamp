15beac58ffd2f71da8fe71b050fb7cb3702a04d2
Nathalia Rippel <nathalia.rippel@gmail.com>
2020-06-23 10:52:24 -0300

    Change code to use hooks to save state.

--- unit03/react-desafio-03-hooks/frontend/src/App.js
+++ unit03/react-desafio-03-hooks/frontend/src/App.js
@@ -1,63 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Spinner from './components/Spinner';
 import Candidates from './components/Candidates';
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      candidates: [],
-      previousVotes: [],
-      previousPercentages: []
-    }
+export default function App() {
+  const [candidates, setCandidates] = useState([]);
+  const [previousVotes, setPreviousVotes] = useState([]);
+  const [previousPercentages, setPreviousPercentages] = useState([]);
 
-    this.interval = null;
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => {
+  useEffect(() => {
+    const interval = setInterval(() => {
       fetch('http://localhost:8080/votes')
         .then((res) => {
           return res.json();
         })
         .then((json) => {
-          const previousVotes = this.state.candidates.map(({ id, votes }) => {
+          const localPreviousVotes = candidates.map(({ id, votes }) => {
             return { id, votes };
           });
 
-          const previousPercentages = this.state.candidates.map(({ id, percentage }) => {
+          const localPreviousPercentages = candidates.map(({ id, percentage }) => {
             return { id, percentage };
           });
 
-          this.setState({
-            candidates: json.candidates,
-            previousVotes,
-            previousPercentages
-          });
+          setCandidates(json.candidates);
+          setPreviousVotes(localPreviousVotes);
+          setPreviousPercentages(localPreviousPercentages);
         });
     }, 1000);
-  }
-
-  render() {
-    const { candidates, previousVotes, previousPercentages } = this.state;
-
-    if (candidates.length === 0) {
-      return (
-        <div className="container">
-          <Spinner description="Carregando..." />
-        </div>
-      );
+    return () => {
+      clearInterval(interval);
     }
+  }, [candidates]);
+
+  if (candidates.length === 0) {
     return (
       <div className="container">
-        <Header title="Votação" />
-        <Candidates
-          candidates={candidates}
-          previousVotes={previousVotes}
-          previousPercentages={previousPercentages}
-        />
+        <Spinner description="Carregando..." />
       </div>
     );
   }
+
+  return (
+    <div className="container">
+      <Header title="Votação" />
+      <Candidates
+        candidates={candidates}
+        previousVotes={previousVotes}
+        previousPercentages={previousPercentages}
+      />
+    </div>
+  );
 }
