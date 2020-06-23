e440c0432e7cf56f0c95bf9c3b01003a4ae77593
Nathalia Rippel <nathalia.rippel@gmail.com>
2020-06-23 10:31:00 -0300

    Change code to use hooks to save state.

--- unit03/react-desafio-02-hooks/src/App.js
+++ unit03/react-desafio-02-hooks/src/App.js
@@ -1,81 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Countries from './components/countries/Countries';
 import Header from './components/header/Header';
 
+export default function App() {
+  const [allCountries, setAllCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [filteredPopulation, setFilteredPopulation] = useState(0);
+  const [userFilter, setUserFilter] = useState('');
 
+  useEffect(() => {
+    const getCountries = async () => {
+      const res = await fetch('https://restcountries.eu/rest/v2/all');
+      const json = await res.json();
 
-export default class App extends Component {
-  constructor() {
-    super();
+      const allCountries = json.map(({ numericCode, translations, flag, population }) => {
+        const name = translations.br;
+        return {
+          id: numericCode,
+          name: name,
+          filterName: name.toLowerCase(),
+          flag,
+          population
+        }
+      });
 
-    this.state = {
-      allCountries: [],
-      filteredCountries: [],
-      filteredPopulation: 0,
-      filter: ''
+      const filteredPopulation = getTotalPopulationFrom(allCountries);
+
+      setAllCountries(allCountries);
+      setFilteredCountries(Object.assign([], allCountries));
+      setFilteredPopulation(filteredPopulation);
     }
-  }
 
-  getTotalPopulationFrom = (countries) => {
+    getCountries();
+  }, [])
+
+  const getTotalPopulationFrom = (countries) => {
     return countries.reduce((acc, cur) => acc + cur.population, 0);
   }
 
-  async componentDidMount() {
-    const res = await fetch('https://restcountries.eu/rest/v2/all');
-    const json = await res.json();
-
-    const allCountries = json.map(({ numericCode, translations, flag, population }) => {
-      const name = translations.br;
-      return {
-        id: numericCode,
-        name: name,
-        filterName: name.toLowerCase(),
-        flag,
-        population
-      }
-    });
-
-    const filteredPopulation = this.getTotalPopulationFrom(allCountries);
-
-    this.setState({
-      allCountries,
-      filteredCountries: allCountries,
-      filteredPopulation
-    });
-  }
-
-  handleFilterChange = (newFilter) => {
-    this.setState({
-      filter: newFilter
-    });
-
+  const handleFilterChange = (newFilter) => {
+    setUserFilter(newFilter);
     const filterLowerCase = newFilter.toLowerCase();
 
-    const filteredCountries = this.state.allCountries.filter((country) => {
+    const filteredCountries = allCountries.filter((country) => {
       return country.filterName.includes(filterLowerCase);
     });
 
-    const filteredPopulation = this.getTotalPopulationFrom(filteredCountries);
+    const filteredPopulation = getTotalPopulationFrom(filteredCountries);
 
-    this.setState({
-      filteredCountries,
-      filteredPopulation
-    });
+    setFilteredCountries(filteredCountries);
+    setFilteredPopulation(filteredPopulation);
   }
 
-  render() {
-    const { filter, filteredCountries, filteredPopulation } = this.state;
-
-    return (
-      <div className="container">
-        <h1 className="center-align">React Countries</h1>
-        <Header
-          filter={filter}
-          countryCount={filteredCountries.length}
-          populationCount={filteredPopulation}
-          onFilterChange={this.handleFilterChange} />
-        <Countries countries={filteredCountries} />
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <h1 className="center-align">React Countries</h1>
+      <Header
+        filter={userFilter}
+        countryCount={filteredCountries.length}
+        populationCount={filteredPopulation}
+        onFilterChange={handleFilterChange} />
+      <Countries countries={filteredCountries} />
+    </div>
+  );
 }
