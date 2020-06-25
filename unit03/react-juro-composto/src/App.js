8257147b1c4e721dc77e81fcecb939909fff7cb0
Nathalia Rippel <nathalia.rippel@gmail.com>
2020-06-25 09:20:21 -0300

    Fix interest formula issue.

--- unit03/react-juro-composto/src/App.js
+++ unit03/react-juro-composto/src/App.js
@@ -1,31 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { getTotal } from './utils/interest';
-import { formatCurrency, formatPercentage } from './utils/formatter';
 import Form from './components/Form';
-
+import { formatCurrency, formatPercentage } from './utils/formatter';
 
 export default function App() {
   const [capital, setCapital] = useState(0);
   const [interestRate, setInterestRate] = useState(0);
   const [period, setPeriod] = useState(0);
-  const [termsInfo, setTermsInfo] = useState([]);
+  const [terms, setTerms] = useState([]);
 
   useEffect(() => {
-    let terms = [];
-    for (let i = 0; i < period; i++) {
-      let total = getTotal(capital, interestRate, period);
-      let interest = total - capital;
-      let percentage = capital > 0 ? (total / capital) - 1 : 0;
+    const newTerms = [];
 
-      terms.push({
-        id: i,
+    for (let idx = 1; idx <= period; idx++) {
+      let total = (capital * (((interestRate / 100) + 1) ** idx)).toFixed(2);
+      let percentage = total > 0 ? (total / capital) - 1 : 0;
+
+      newTerms.push({
+        id: idx,
         total,
-        interest,
-        percentage
+        interest: total - capital,
+        percentage,
       });
     }
 
-    setTermsInfo(terms);
+    setTerms(newTerms);
   }, [capital, interestRate, period]);
 
   const onFormChange = (newCapital, newInterestRate, newPeriod) => {
@@ -54,7 +52,7 @@ export default function App() {
       />
       <div className="terms row">
         {
-          termsInfo.map((term) => {
+          terms.map((term) => {
             return (term.total > 0 &&
               <div key={term.id} className="col s2">
                 <div className="row valign-wrapper">
--- unit03/react-juro-composto/src/utils/interest.js
+++
@@ -1,5 +0,0 @@
-export function getTotal(capital, interestRate, term) {
-  let percentage = ((interestRate / 100) + 1).toFixed(3);
-  let total = (capital * (percentage ** term)).toFixed(2);
-  return total;
-}
